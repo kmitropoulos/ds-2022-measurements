caaf5d46300ca8e74ed93184c0becf6f5b3af722
Konstantinos - Christos Mitropoulos <kostismitropoulos@gmail.com>
2022-12-23 23:09:19 +0100

    updated analysis scripts

--- an2.py
+++ an2.py
@@ -2,6 +2,9 @@ fpfiles = ["fp1.txt", "fp2.txt", "fp3.txt", "fp4.txt"]
 fpddfiles = ["fpdd1.txt", "fpdd2.txt", "fpdd3.txt", "fpdd4.txt"]
 fffiles = ["ff1.txt", "ff2.txt", "ff3.txt", "ff4.txt"]
 ddfiles = ["dd1.txt", "dd2.txt", "dd3.txt", "dd4.txt"]
+labelsize = 14
+titlesize = 16
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -46,11 +49,11 @@ def latency_plot(res, exp):
     xpos = np.arange(len(x))
     fig, ax = plt.subplots()
     ax.errorbar(xpos, y, yerr=e, fmt='-o', ecolor="red", capsize=5, barsabove=False)
-    ax.set_ylabel("Average latency (s)")
-    ax.set_xlabel("Parallel requests")
+    ax.set_ylabel("Average latency (s)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
     ax.set_xticks(xpos)
     ax.set_xticklabels(x)
-    ax.set_title(f"Average completion latency - exp {exp}")
+    ax.set_title(f"Average Execution Latency - exp {exp}", fontsize=titlesize, wrap=True)
     ax.grid(True)
     plt.savefig(f'images/lat_av_{exp}.png')
     plt.show()
@@ -67,33 +70,49 @@ def joined_latency(res1,res2, exp1, exp2):
     fig, ax = plt.subplots()
     ax.errorbar(xpos, y1, yerr=e1, label={exp1},  fmt='-o', ecolor="red", capsize=5, barsabove=False)
     ax.errorbar(xpos, y2, yerr=e2, label={exp2}, fmt='--s', ecolor="green", capsize=5, barsabove=False)
-    ax.set_ylabel("Average latency (s)")
-    ax.set_xlabel("Parallel requests")
+    ax.set_ylabel("Average latency (s)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
     ax.set_xticks(xpos)
     ax.set_xticklabels(x)
-    ax.set_title(f"Average completion latency - exp {exp1} / {exp2}")
+    ax.set_title(f"Average Execution Latency - exp {exp1} / {exp2}", fontsize=titlesize, wrap=True)
     ax.grid(True)
     plt.legend()
     plt.savefig(f'images/lat_av_comp_{exp1}_{exp2}.png')
     plt.show()
 
-def pdvc(res,exp):
+def pd(res,exp):
     x = np.array(list(res.keys()))
     y = np.array([sum([abs(j - st.mean(res[x]))/st.mean(res[x]) for j in res[x]])/len(res[x]) for x in res.keys()])
     xpos = np.arange(len(x))
     fig, ax = plt.subplots()
     bars = ax.bar(xpos, y)
-    ax.set_ylabel("Deviation (normalized)")
-    ax.set_xlabel("Parallel requests")
+    ax.set_ylabel("Deviation (ratio)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
     ax.set_xticks(xpos)
     ax.set_xticklabels(x)
-    ax.set_title(f"Percentage Deviation of Virtual Cores - exp {exp}")
+    ax.set_title(f"Average Percentage Deviation of Execution latency - exp {exp}", fontsize=titlesize, wrap=True)
     ax.grid(True)
     ax.set_axisbelow(True)
-    plt.savefig(f'images/cm_pdvc_{exp}.png')
+    plt.savefig(f'images/cm_pd_{exp}.png')
     plt.show()
 
-def joined_pdvc(res1,res2, exp1, exp2):
+def ad(res,exp):
+    x = np.array(list(res.keys()))
+    y = np.array([sum([abs(j - st.mean(res[x])) for j in res[x]])/len(res[x]) for x in res.keys()])
+    xpos = np.arange(len(x))
+    fig, ax = plt.subplots()
+    bars = ax.bar(xpos, y)
+    ax.set_ylabel("Deviation (s)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
+    ax.set_xticks(xpos)
+    ax.set_xticklabels(x)
+    ax.set_title(f"Average Absolute Deviation of Execution latency - exp {exp}", fontsize=titlesize, wrap=True)
+    ax.grid(True)
+    ax.set_axisbelow(True)
+    plt.savefig(f'images/cm_ad_{exp}.png')
+    plt.show()
+
+def joined_pd(res1,res2, exp1, exp2):
     mn = min([len(res1), len(res2)])
     x = np.array(list(res1.keys())[:mn])
     y1 = np.array([sum([abs(j - st.mean(res1[x]))/st.mean(res1[x]) for j in res1[x]])/len(res1[x]) for x in list(res1.keys())[:mn]])
@@ -102,35 +121,64 @@ def joined_pdvc(res1,res2, exp1, exp2):
     fig, ax = plt.subplots()
     bars1 = ax.bar(xpos-0.2, y1, width=0.4, label=exp1)
     bars2 = ax.bar(xpos+0.2, y2, width=0.4, label=exp2)
-    ax.set_ylabel("Deviation (normalized)")
-    ax.set_xlabel("Parallel requests")
+    ax.set_ylabel("Deviation (ratio)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
     ax.set_xticks(xpos)
     ax.set_xticklabels(x)
-    ax.set_title(f"Percentage Deviation of Virtual Cores - exp {exp1}/{exp2}")
+    ax.set_title(f"Average Percentage Deviation of Execution latency - exp {exp1}/{exp2}", fontsize=titlesize, wrap=True)
     ax.grid(True)
     ax.set_axisbelow(True)
     plt.legend()
     # ax.bar_label(bars, ['%.2f' % i for i in y], padding=2, color='r', fontsize=10)
-    plt.savefig(f'images/joined_pdvc_{exp1}_{exp2}.png')
+    plt.savefig(f'images/joined_pd_{exp1}_{exp2}.png')
+    plt.show()
+
+def joined_ad(res1,res2, exp1, exp2):
+    mn = min([len(res1), len(res2)])
+    x = np.array(list(res1.keys())[:mn])
+    y1 = np.array([sum([abs(j - st.mean(res1[x])) for j in res1[x]])/len(res1[x]) for x in list(res1.keys())[:mn]])
+    y2 = np.array([sum([abs(j - st.mean(res2[x])) for j in res2[x]])/len(res2[x]) for x in list(res1.keys())[:mn]])
+    xpos = np.arange(len(x))
+    fig, ax = plt.subplots()
+    bars1 = ax.bar(xpos-0.2, y1, width=0.4, label=exp1)
+    bars2 = ax.bar(xpos+0.2, y2, width=0.4, label=exp2)
+    ax.set_ylabel("Deviation (s)", fontsize=labelsize)
+    ax.set_xlabel("Parallel requests", fontsize=labelsize)
+    ax.set_xticks(xpos)
+    ax.set_xticklabels(x)
+    ax.set_title(f"Average Absolute Deviation of Execution latency - exp {exp1}/{exp2}", fontsize=titlesize, wrap=True)
+    ax.grid(True)
+    ax.set_axisbelow(True)
+    plt.legend()
+    # ax.bar_label(bars, ['%.2f' % i for i in y], padding=2, color='r', fontsize=10)
+    plt.savefig(f'images/joined_ad_{exp1}_{exp2}.png')
     plt.show()
     
 
 
 fpres = get_measurements(fpfiles)
 fpddres = get_measurements(fpddfiles)
+ffres = get_measurements(fffiles)
+ddres = get_measurements(ddfiles)
 
-# for i in fpres.keys():
-#     print(i, len(fpres[i]))
-# for i in fpddres.keys():
-#     print(i, len(fpddres[i]))
 
-# latency_plot(fpres, "fp")
-# latency_plot(fpddres, "fp-dd")
-# latency_plot(get_measurements(fffiles), "ff")
-# latency_plot(get_measurements(ddfiles), "dd")
-# joined_latency(fpres, fpddres, "fp", "fp-dd")
-pdvc(fpres, "fp")
-pdvc(fpddres, "fpdd")
-joined_pdvc(fpres, fpddres, "fp", "fpdd")
-# pdvc(get_measurements(fffiles), "ff")
-# pdvc(get_measurements(ddfiles), "dd")
+
+latency_plot(fpres, "fp")
+latency_plot(fpddres, "fp-dd")
+latency_plot(ffres, "ff")
+latency_plot(ddres, "dd")
+
+joined_latency(fpres, fpddres, "fp", "fp-dd")
+
+pd(fpres, "fp")
+pd(fpddres, "fp-dd")
+pd(ffres, "ff")
+pd(ddres, "dd")
+
+ad(fpres, "fp")
+ad(fpddres, "fp-dd")
+ad(ffres, "ff")
+ad(ddres, "dd")
+
+joined_pd(fpres, fpddres, "fp", "fp-dd")
+joined_ad(fpres, fpddres, "fp", "fp-dd")
--- analysis.py
+++ analysis.py
@@ -1,11 +1,18 @@
-files = ["fp1.txt", "fp2.txt", "fp3.txt", "fp4.txt"]
+fpfiles = ["fp1.txt", "fp2.txt", "fp3.txt", "fp4.txt"]
+fpddfiles = ["fpdd1.txt", "fpdd2.txt", "fpdd3.txt", "fpdd4.txt"]
+fffiles = ["ff1.txt", "ff2.txt", "ff3.txt", "ff4.txt"]
+ddfiles = ["dd1.txt", "dd2.txt", "dd3.txt", "dd4.txt"]
 titles = ["fp"]
+labelsize = 14
+titlesize = 16
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics as st
 import math
 
-def get_measurements():
+def get_measurements(files):
     res = dict()
     for file in files:
         res[file] = dict()
@@ -41,11 +48,11 @@ def latency_plot(res):
 
         fig, ax = plt.subplots()
         ax.errorbar(xpos, y, yerr=e, fmt='-o', ecolor="red", capsize=5, barsabove=False)
-        ax.set_ylabel("Average latency (s)")
-        ax.set_xlabel("Parallel requests")
+        ax.set_ylabel("Average latency (s)", fontsize=labelsize)
+        ax.set_xlabel("Parallel requests", fontsize=labelsize)
         ax.set_xticks(xpos)
         ax.set_xticklabels(x)
-        ax.set_title(f"Average completion latency - exp {i}")
+        ax.set_title(f"Average execution latency - exp {i}", fontsize=titlesize, wrap=True)
         ax.grid(True)
         plt.savefig(f'images/lat_{i.split(".")[0]}.png')
         plt.show()
@@ -58,7 +65,7 @@ def latency_plot(res):
         # plt.grid()
         
 
-def pdvc(res):
+def pd(res,titles):
     data = dict()
 
     for i in res[list(res.keys())[0]].keys():
@@ -73,35 +80,75 @@ def pdvc(res):
 
         fig, ax = plt.subplots()
         bars = ax.bar(xpos, y)
-        ax.set_ylabel("Deviation")
-        ax.set_xlabel("Parallel requests")
+        ax.set_ylabel("Deviation (ratio)", fontsize=labelsize)
+        ax.set_xlabel("Parallel requests", fontsize=labelsize)
         ax.set_xticks(xpos)
         ax.set_xticklabels(x)
-        ax.set_title(f"Percentage Deviation of Virtual Cores - exp {i}")
+        ax.set_title(f"Average Percentage Deviation of Execution latency - exp {i}", fontsize=titlesize, wrap=True)
         ax.grid(True)
         ax.set_axisbelow(True)
         # ax.bar_label(bars, ['%.2f' % i for i in y], padding=2, color='r', fontsize=10)
-        plt.savefig(f'images/pdvc_{i.split(".")[0]}.png')
+        plt.savefig(f'images/pd_{i.split(".")[0]}.png')
         plt.show()
-    print(data)
-    fig, ax = plt.subplots()
-    x = np.array(list(data.keys()))
-    y = np.array([st.mean(data[i]) for i in data.keys()])
-    e = np.array([st.stdev(data[i]) for i in data.keys()])
-    xpos = np.arange(len(x))
-    bars = ax.bar(xpos, y, yerr=e, align='center', alpha=0.7, ecolor='black', capsize=8)
-    ax.set_ylabel("Average Deviation")
-    ax.set_xlabel("Parallel requests")
-    ax.set_xticks(xpos)
-    ax.set_xticklabels(x)
-    ax.set_title(f"Average PDVC - exp: {titles[0]}")
-    ax.grid(True)
-    ax.set_axisbelow(True)
-    plt.savefig(f'images/tot_pdvc_{titles[0]}.png')
-    plt.show()
+
+def ad(res,titles):
+    data = dict()
+
+    for i in res[list(res.keys())[0]].keys():
+        data[i] = []
+    for i in res.keys():
+        x = np.array(list(res[i].keys()))
+        y = np.array([sum([abs(j - st.mean(res[i][x])) for j in res[i][x]])/len(res[i][x]) for x in res[i].keys()])
+        for lb, d in zip(list(res[i].keys()), y):
+            # print(lb, d)
+            data[lb].append(d)
+        xpos = np.arange(len(x))
+
+        fig, ax = plt.subplots()
+        bars = ax.bar(xpos, y)
+        ax.set_ylabel("Deviation (s)", fontsize=labelsize)
+        ax.set_xlabel("Number of Parallel requests", fontsize=labelsize)
+        ax.set_xticks(xpos)
+        ax.set_xticklabels(x)
+        ax.set_title(f"Average Absolute Deviation of Execution latency - exp {i}", fontsize=titlesize, wrap=True)
+        ax.grid(True)
+        ax.set_axisbelow(True)
+        # ax.bar_label(bars, ['%.2f' % i for i in y], padding=2, color='r', fontsize=10)
+        plt.savefig(f'images/ad_{i.split(".")[0]}.png')
+        plt.show()
+
+    
+    # print(data)
+    # fig, ax = plt.subplots()
+    # x = np.array(list(data.keys()))
+    # y = np.array([st.mean(data[i]) for i in data.keys()])
+    # e = np.array([st.stdev(data[i]) for i in data.keys()])
+    # xpos = np.arange(len(x))
+    # bars = ax.bar(xpos, y, yerr=e, align='center', alpha=0.7, ecolor='black', capsize=8)
+    # ax.set_ylabel("Average Deviation")
+    # ax.set_xlabel("Parallel requests")
+    # ax.set_xticks(xpos)
+    # ax.set_xticklabels(x)
+    # ax.set_title(f"Average PDVC - exp: {titles}")
+    # ax.grid(True)
+    # ax.set_axisbelow(True)
+    # plt.savefig(f'images/tot_pdvc_{titles}.png')
+    # plt.show()
     
 
 
 
-latency_plot(get_measurements())
-pdvc(get_measurements())
+fp = get_measurements(fpfiles)
+ff = get_measurements(fffiles)
+dd = get_measurements(ddfiles)
+
+latency_plot(fp)
+latency_plot(ff)
+latency_plot(dd)
+pd(fp, "fp")
+pd(ff, "ff")
+pd(dd, "dd")
+ad(fp, "fp")
+ad(ff, "ff")
+ad(dd, "dd")
+
